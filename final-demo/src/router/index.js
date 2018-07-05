d711f80a97783e6e922de0c25b0d099e9a77a466
zailushang1996 <2212926237@qq.com>
2018-07-05 14:25:40 +0800

    final-demo搭建 json-server mock数据 代理

---
+++ final-demo/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import IndexPage from '../pages/index'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/',
+      name: 'IndexPage',
+      component: IndexPage
+    },
+
+  ]
+})
