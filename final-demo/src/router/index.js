d208f5bbb13c9f1d8a84c848995905ea2946f647
zailushang1996 <2212926237@qq.com>
2018-07-09 08:30:47 +0800

    4个detail界面

--- final-demo/src/router/index.js
+++ final-demo/src/router/index.js
@@ -1,17 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../pages/index'
+import DetailPage from '../pages/detail'
+import DetailAnaPage from '../pages/detail/analysis'
+import DetailCouPage from '../pages/detail/count'
+import DetailForPage from '../pages/detail/forecast'
+import DetailPubPage from '../pages/detail/publish'
+
 
 Vue.use(Router)
 
 export default new Router({
-  mode:'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'IndexPage',
       component: IndexPage
     },
+    {
+      path: '/detail',
+      component: DetailPage,
+      redirect:'/detail/analysis',
+      children: [{
+        path: 'analysis',
+        component: DetailAnaPage
+      },
+        {
+          path: 'forecast',
+          component: DetailForPage
+        },
+        {
+          path:'count',
+          component:DetailCouPage
+        },
+        {
+          path:'publish',
+          component:DetailPubPage
+        }
+      ]
+    }
 
   ]
 })
